d664c213a51610421445b840303b55dfec4021fc
nishu-murmu <nishumurmu017@gmail.com>
2022-05-06 17:51:52 +0530

    (feat): adding user profile page

---
+++ src/pages/Auth/AccountPage.js
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/react';
+
+// files
+import Account from '../../components/Auth/Account';
+
+const AccountPage = () => {
+  return (
+    <Box>
+      <Account />
+    </Box>
+  );
+};
+
+export default AccountPage;
