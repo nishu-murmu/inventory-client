d412e14b05477adff2d45c5539913f1523eac0ce
nishu-murmu <nishumurmu017@gmail.com>
2022-05-30 09:22:43 +0530

    feat(header): adding links to icons

--- src/sections/main-header-section.js
+++ src/sections/main-header-section.js
@@ -43,9 +43,11 @@ const MainHeader = () => {
       </Link>
       <Spacer />
       <Flex spacing={5} gridColumnGap={4}>
-        <Button variant={'ghost'}>
-          <Icon as={FaGithub} />
-        </Button>
+        <a href={'https://github.com/nishu-murmu/inventory-client'}>
+          <Button>
+            <Icon as={FaGithub} />
+          </Button>
+        </a>
         <ColorModeSwitcher justifySelf="flex-end" />
         <Menu>
           <MenuButton as={Button} cursor={'pointer'} variant={'ghost'}>
